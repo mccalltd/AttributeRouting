959659a9293fd4c5417b52cc5217f2b350794b4d
Tim McCall <mccalltd@gmail.com>
2010-08-17 23:36:45 -0600

    Added versioning to AttributeRouting assembly. Cleared some old crud from a former project name.

--- AttributeRouting/Properties/AssemblyInfo.cs
+++ AttributeRouting/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MvcSinatra.Framework")]
+[assembly: AssemblyTitle("AttributeRouting")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MvcSinatra.Framework")]
+[assembly: AssemblyProduct("AttributeRouting")]
 [assembly: AssemblyCopyright("Copyright ©  2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.*")]
+//[assembly: AssemblyFileVersion("0.1.*")]
