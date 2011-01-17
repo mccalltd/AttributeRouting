fd44169d4aab942a1ff0797645087ad4b1f23b74
Tim McCall <mccalltd@gmail.com>
2011-01-17 01:12:35 -0700

    Forgot to get rid of old things.

--- AttributeRouting.Specs/Features/Logging.feature
+++
@@ -1,6 +0,0 @@
-﻿Feature: Logging
-
-Scenario: Log the routes to the standard output
-	Given I generate the routes defined in the subject controllers
-	When I log the routes
-	Then ta-da!
--- AttributeRouting.Web/Areas/Admin/Controllers/AdminControllerBase.cs
+++
@@ -1,9 +0,0 @@
-﻿using AttributeRouting.Web.Controllers;
-
-namespace AttributeRouting.Web.Areas.Admin.Controllers
-{
-    [RouteArea("Admin")]
-    public class AdminControllerBase : ControllerBase
-    {
-    }
-}
