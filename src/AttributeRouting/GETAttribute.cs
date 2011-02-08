f7f46844a7e28c1567198f5ab561879cfdba8a2b
Tim McCall <mccalltd@gmail.com>
2011-02-08 12:34:49 -0700

    Fixed xml-doc for GETAttribute.

--- src/AttributeRouting/GETAttribute.cs
+++ src/AttributeRouting/GETAttribute.cs
@@ -6,7 +6,7 @@
     public class GETAttribute : RouteAttribute
     {
         /// <summary>
-        /// Specify a route for an action constrained to requests providing an httpMethod value of POST.
+        /// Specify a route for an action constrained to requests providing an httpMethod value of GET.
         /// </summary>
         /// <param name="url">The url that is associated with this action</param>
         public GETAttribute(string url) : base(url, "GET") {}
