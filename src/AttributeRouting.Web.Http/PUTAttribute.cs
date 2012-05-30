7348ffa1efff667face01c2a4a65c001c1c64f6e
Rob Beal <rob.beal@7digital.com>
2012-05-30 16:55:21 +0100

    Added OPTIONS httpmethod to all attributes so that a client can query the allowed verbs for a resource

--- src/AttributeRouting.Web.Http/PUTAttribute.cs
+++ src/AttributeRouting.Web.Http/PUTAttribute.cs
@@ -11,6 +11,6 @@ namespace AttributeRouting.Web.Http
         /// Specify a route for an action constrained to requests providing an httpMethod value of PUT.
         /// </summary>
         /// <param name="routeUrl">The url that is associated with this action</param>
-        public PUTAttribute(string routeUrl) : base(routeUrl, HttpMethod.Put) {}
+		public PUTAttribute(string routeUrl) : base(routeUrl, HttpMethod.Put, HttpMethod.Options) { }
     }
 }
