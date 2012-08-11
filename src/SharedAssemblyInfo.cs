c63de423ac17008569ac8592d58e5068618def65
Tim McCall <mccalltd@gmail.com>
2012-08-10 19:22:23 -0600

    updated readme and assembly info for v2.5.0-pre

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -9,4 +9,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("2.5")]
+//[assembly: AssemblyVersion("2.5.0.0")]
+[assembly: AssemblyInformationalVersion("2.5.0-pre")]
