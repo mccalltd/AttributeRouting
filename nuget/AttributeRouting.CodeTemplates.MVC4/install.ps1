d3441c5388d872b72d5fbb677ce6c961972f2707
Tim McCall <mccalltd@gmail.com>
2012-08-25 02:15:02 -0600

    #79 - removed addview t4 templates. trying to get code template nuget package to install the correct templates (csharp vs vb) depending on project type.

---
+++ nuget/AttributeRouting.CodeTemplates.MVC4/install.ps1
@@ -0,0 +1,31 @@
+﻿param($installPath, $toolsPath, $package, $project)
+
+# Remove the templates for other languages
+if ($project.Type -eq "C#") {	
+	$path = [System.IO.Path]
+	$projectDirectory = $path::GetDirectoryName($project.FullName)
+	$codeTemplatesDirectory = $projectDirectory + "\CodeTemplates"
+
+	# delete vb dir
+	Remove-Item -Path ($codeTemplatesDirectory + "\VisualBasic") -Recurse
+
+	# move the csharp stuff into proper position
+	Move-Item -Path ($codeTemplatesDirectory + "\CSharp\AddController") -Destination ($codeTemplatesDirectory + "\AddController")
+	
+	# delete csharp dir
+	Remove-Item -Path ($codeTemplatesDirectory + "\CSharp") -Recurse
+}
+
+$codeTemplatesFolder = $project.ProjectItems.Item("CodeTemplates");
+
+# ASP.NET MVC uses a custom Text Template Host to scaffold views and controllers so clear the built in TextTemplatingFileGenerator from the Custom Tool property.
+foreach($ttFile in $codeTemplatesFolder.ProjectItems.Item("AddController").ProjectItems){
+	$ttFile.Properties.Item("CustomTool").Value = "";
+}
+
+foreach($ttLanguageFolder in $codeTemplatesFolder.ProjectItems.Item("AddView").ProjectItems){
+	foreach($ttFile in $ttLanguageFolder.ProjectItems){
+		$ttFile.Properties.Item("CustomTool").Value = "";
+	}
+}
+
