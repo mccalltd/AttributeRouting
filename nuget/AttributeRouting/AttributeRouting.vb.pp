4248ab846321074b51532bab7912a263630eca38
Tim McCall <mccalltd@gmail.com>
2012-08-26 21:46:56 -0600

    #116 - some app_start love for vb projects :). added default AR hooks in vb, with a powershell script to remove the vb/c# file depending on the project type.

---
+++ nuget/AttributeRouting/AttributeRouting.vb.pp
@@ -0,0 +1,20 @@
+﻿Imports System.Web.Routing
+Imports AttributeRouting.Web.Mvc
+
+<assembly: WebActivator.PreApplicationStartMethod(GetType($rootnamespace$.App_Start.AttributeRouting), "Start")>
+
+Namespace $rootnamespace$.App_Start
+    Public Class AttributeRouting
+		Public Shared Sub RegisterRoutes(routes As RouteCollection)
+            
+			' See http://github.com/mccalltd/AttributeRouting/wiki for more options.
+			' To debug routes locally using the built in ASP.NET development server, go to /routes.axd
+            
+			routes.MapAttributeRoutes()
+		End Sub
+
+        Public Shared Sub Start()
+            RegisterRoutes(RouteTable.Routes)
+        End Sub
+    End Class
+End Namespace
