43bcd9891a580ea29e3fc1b485c21a358512c2dd
gregmac <github@list.mtechsolutions.ca>
2013-01-16 12:25:34 -0500

    Add HttpGet to VersionedController (since it's using non-conventional names), and add example of different controller actions for different versions of the same URL

--- src/AttributeRouting.Tests.Web/Areas/Api/Controllers/VersionedController.cs
+++ src/AttributeRouting.Tests.Web/Areas/Api/Controllers/VersionedController.cs
@@ -1,4 +1,7 @@
-﻿using AttributeRouting.Web.Http;
+﻿using System;
+using System.Web.Http;
+using AttributeRouting.Tests.Web.Models;
+using AttributeRouting.Web.Http;
 
 namespace AttributeRouting.Tests.Web.Areas.Api.Controllers
 {
@@ -7,23 +10,41 @@ namespace AttributeRouting.Tests.Web.Areas.Api.Controllers
     {
         //
         // GET: /Versioned/
-        [GET("Versioned", MinVer = "0.0")]
-        public string Index()
+        [HttpGet, GET("Versioned", MaxVer = "1.1")]
+        public VersionedModel_old Index_old()
         {
-            return "This is /versioned";
+            return new VersionedModel_old() {Text = "This is /versioned (up to 1.1)", GeneratedTime = DateTime.Now};
         }
 
-        [GET("Versioned/{id}", MinVer="1.1")]
+        [HttpGet, GET("Versioned", MinVer = "1.2")]
+        public VersionedModel Index()
+        {
+            return new VersionedModel()
+                {
+                    Title = "This is /versioned", 
+                    Body = "This model is added in 1.2, and returns title/body isntead of just text", 
+                    GeneratedTime = DateTime.Now
+                };
+        }
+
+        [HttpGet, GET("Versioned/{id}", MinVer = "1.1")]
         public string Show(int id)
         {
             return string.Format("This is /versioned/id with id = {0}", id);
         }
 
-        [GET("Versioned/SingleVersion", MinVer="1.0", MaxVer="1.0")]
+        [HttpGet, GET("Versioned/SingleVersion", MinVer = "1.0", MaxVer = "1.0")]
         public string New()
         {
             return "This should only work with version 1.0";
         }
+
+        [HttpGet, GET("Versioned/BeforeV1", MinVer = "0.0")]
+        public string BeforeV1()
+        {
+            return "This existed in versions even prior to 1.0 (overrides class-level version)";
+        }
+
 
     }
 }
---
+++ src/AttributeRouting.Tests.Web/Models/VersionedModels.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AttributeRouting.Tests.Web.Models
+{
+    public class VersionedModel_old
+    {
+        public string Text { get; set; }
+        public DateTime GeneratedTime { get; set; } 
+    }
+
+    public class VersionedModel
+    {
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public DateTime GeneratedTime { get; set; } 
+    }
+}
