ad153213504b97b67e9d136440af31f947f49e7b
Tim McCall <mccalltd@gmail.com>
2013-03-02 14:04:57 -0700

    test controllers for web api action inheritence.

---
+++ src/AttributeRouting.Tests.Web/Areas/Api/Controllers/InheritActionsControllers.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Http;
+using AttributeRouting.Web.Http;
+
+namespace AttributeRouting.Tests.Web.Areas.Api.Controllers
+{
+    public abstract class InheritActionsApiControllerBase : BaseApiController
+    {
+        [GET("Base-Method"), HttpGet]
+        public string Index()
+        {
+            return "Base Index";
+        }
+    }
+
+    [RoutePrefix("Inherit/Derived")]
+    public class InheritActionsDerivedApiController : InheritActionsApiControllerBase {}
+}
