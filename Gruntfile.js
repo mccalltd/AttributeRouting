f13dbfa47d115c25d833d5ba40333f151120a504
Tim McCall <mccalltd@gmail.com>
2013-06-30 23:32:51 -0600

    grunting shared assembly info

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,12 +1,17 @@
 module.exports = function (grunt) {
   var fs = require('fs')
+    , prompt = require('prompt')
     , wrench = require('wrench')
 
-    , DIRECTORIES = {
+    , directories = {
       build: __dirname + '\\build',
       out: __dirname + '\\build\\bin',
       src: __dirname + '\\src'
     }
+    , files = {
+      assemblyInfo: directories.src + '\\SharedAssemblyInfo.cs'
+    }
+    , version
   ;
 
   //======================================
@@ -15,6 +20,7 @@ module.exports = function (grunt) {
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    
     msbuild: {
       release: {
         src: ['src\\AttributeRouting.sln'],
@@ -24,18 +30,19 @@ module.exports = function (grunt) {
           stdout: true,
           buildParameters: {
             WarningLevel: 2,
-            OutputPath: DIRECTORIES.out
+            OutputPath: directories.out
           },
           verbosity: 'minimal'
         }
       }
     },
+    
     shell: {
       test: {
         command: [
           'tools\\nunit\\nunit-console-x86.exe',
-          DIRECTORIES.out + '\\AttributeRouting.Specs.dll',
-          '/work:' + DIRECTORIES.build,
+          directories.out + '\\AttributeRouting.Specs.dll',
+          '/work:' + directories.build,
           '/out:TestResults.txt',
           '/result:TestResults.xml',
           '/nologo',
@@ -59,19 +66,80 @@ module.exports = function (grunt) {
   // Custom tasks
   //======================================
 
+  grunt.registerTask('prompt', 'Prompts for required build params.', function () {
+    var done = this.async();
+    var defaultVersion;
+    var schema;
+
+    fs.readFile(files.assemblyInfo, function(err, data) {
+      if (err) throw err;
+
+      defaultVersion = /AssemblyInformationalVersion\("(.*)"\)/.exec(data.toString())[1];
+
+      prompt.start();
+
+      schema = {
+        properties: {
+          version: {
+            default: defaultVersion,
+            pattern: /^\d+\.\d+(\.\d+){0,2}(-\S)?$/,
+            message: 'version must be a valid semantic version.',
+            required: true
+          }
+        }
+      };
+      
+      prompt.get(schema, function (err, result) {
+        if (err) throw err;
+        version = result.version;
+        done();
+      });
+    });
+  });
+  
   grunt.registerTask('clean', 'Cleans the build directory.', function () {
     var done = this.async();
 
-    console.log('cleaning', DIRECTORIES.build);
-
-    wrench.rmdirRecursive(DIRECTORIES.build, function () {
-      fs.mkdir(DIRECTORIES.build, done);
+    wrench.rmdirRecursive(directories.build, function () {
+      fs.mkdir(directories.build, done);
     });
   });
 
+  grunt.registerTask('assemblyInfo', "Creates the shared AssemblyInfo.cs file.", function () {
+    grunt.task.requires('prompt');
+    
+    var done = this.async();
+    var nonPrereleaseVersion = version.split('-')[0];
+    var fileContent = [
+      'using System;',
+      'using System.Reflection;',
+      'using System.Runtime.InteropServices;',
+      '',
+      '[assembly: ComVisible(false)]',
+      '[assembly: AssemblyCompany("")]',
+      '[assembly: AssemblyProduct("AttributeRouting")]',
+      '[assembly: AssemblyCopyright("Copyright 2010-' + new Date().getFullYear() + ' Tim McCall")]',
+      '[assembly: AssemblyTrademark("")]',
+      '[assembly: AssemblyVersion("'+ nonPrereleaseVersion + '")]',
+      '[assembly: AssemblyFileVersion("'+ nonPrereleaseVersion + '")]',
+      '[assembly: AssemblyInformationalVersion("'+ version + '")]',
+      '[assembly: AssemblyConfiguration("Release")]'
+    ].join('\r\n');
+    
+    fs.writeFile(files.assemblyInfo, fileContent, done);
+  });
+
+  grunt.registerTask('nugetPack', 'Creates nuget packages from build output.', function() {
+
+  });
+  
   //======================================
   // Tasks
   //======================================
 
-  grunt.registerTask('default', ['clean', 'msbuild', 'shell:test']);
+  grunt.registerTask('default', ['build']);
+  
+  grunt.registerTask('build', ['prompt', 'clean', 'msbuild', 'assemblyInfo', 'shell:test']);
+
+  grunt.registerTask('package', ['build', 'nugetPack']);
 };
